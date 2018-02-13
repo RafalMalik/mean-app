073925d27f347e52419ecdd02343828b229221f4
Rafal Malik <rafalmalik.info@gmail.com>
2018-02-13 14:05:43 +0100

    Gotowa apka.

--- app.js
+++ app.js
@@ -4,15 +4,37 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var bluebird = require('bluebird')
 
 var index = require('./routes/index');
 var users = require('./routes/users');
+
+var api = require('./routes/api.route')
 
 var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
+
+var mongoose = require('mongoose')
+mongoose.Promise = bluebird
+mongoose.connect('mongodb://193.70.113.241:27017/todoapp', { useMongoClient: true })
+  .then(() => {
+    console.log(`Succesfully Connected to the
+Mongodb Database  at URL : mongodb://127.0.0.1:27017/todoapp`)
+  })
+  .catch(() => {
+    console.log(`Error Connecting to the Mongodb 
+Database at URL : mongodb://127.0.0.1:27017/todoapp`)
+  })
+
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "http://localhost:4200");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+  next();
+});
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
@@ -24,16 +46,19 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
 app.use('/users', users);
+app.use('/api', api);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   var err = new Error('Not Found');
   err.status = 404;
   next(err);
 });
 
+
+
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
